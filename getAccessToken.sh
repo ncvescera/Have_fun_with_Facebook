00a1512d6c731785df2e90371763efaf3a979f21
root <root@blackforce.blackforce.irpi.cnr.it>
2016-09-30 10:00:16 +0200

    Aggiunti file per le credenziali

--- getAccessToken.sh
+++ getAccessToken.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-curl -X GET "https://graph.facebook.com/v2.7/oauth/access_token?client_id=1144363815642418&client_secret=93c7ddfaf5a216b8c62972ab12de2c86&grant_type=client_credentials" > secret.json
+source credentialToken.sh
+curl -X GET "https://graph.facebook.com/v2.7/oauth/access_token?client_id=$id&client_secret=$secret&grant_type=client_credentials" > secret.json
