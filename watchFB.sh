2703e0f101151092b508f55ce50e749fae70940d
root <root@blackforce.blackforce.irpi.cnr.it>
2016-09-30 09:51:41 +0200

    Aggiunti controlli al file watchFB.sh

--- watchFB.sh
+++ watchFB.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]
+then
+	echo "Argomenti passati 0, richiesti 1"
+	echo "./watchFB id_pagina"
+	exit
+fi
+
 echo "Getting Access Token ..."
 ./getAccessToken.sh
 echo "Decoding Access Token ..."
@@ -13,8 +20,4 @@ echo "Posts: "
 ./parseData.py
 echo "Distinct Table ..."
 ./distinctTable.py
-echo "Done !"
-echo "Sharing posts ..."
-#cd autoPost
-#./autoPost.py
 echo "Done !"
